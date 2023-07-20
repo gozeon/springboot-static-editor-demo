62a67ea23758a3e53d8cd41ee004be5f6be05536
gozeon <goze.qiu@gmail.com>
2023-07-20 11:11:17 +0800

    chore: init editor api

--- src/main/java/com/example/demo/playground/PlayController.java
+++ src/main/java/com/example/demo/playground/PlayController.java
@@ -1,16 +1,24 @@
 package com.example.demo.playground;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("play")
@@ -35,11 +43,20 @@ public class PlayController {
     }
 
     private String getParentPath(String path) {
-        if(path.indexOf("/") == -1) {
+        if (path.indexOf("/") == -1) {
             return "";
         } else {
             return path.substring(0, path.lastIndexOf("/"));
         }
+    }
+
+    private ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", message);
+        map.put("status", status.value());
+        map.put("data", responseObj);
+
+        return new ResponseEntity<Object>(map, status);
     }
 
     @GetMapping("")
@@ -55,5 +72,25 @@ public class PlayController {
         model.addAttribute("basepath", isRoot ? "" : formatPath + "/");
         model.addAttribute("fileList", resources);
         return "play/index";
+    }
+
+    @GetMapping("file")
+    @ResponseBody
+    public ResponseEntity<Object> getFileInfo(@RequestParam(name = "path") String path)
+            throws IOException {
+        Resource resource = new ClassPathResource(path);
+        if (!resource.exists()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        File file = resource.getFile();
+        Path filepath = file.toPath();
+        String content = new String(Files.readAllBytes(filepath));
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("path", path);
+        map.put("content", content);
+        map.put("mimeType", Files.probeContentType(filepath));
+        map.put("extension", path.substring(path.lastIndexOf(".")));
+
+        return this.generateResponse("ok", HttpStatus.OK, map);
     }
 }
--- src/main/resources/static/play/main.js
+++ src/main/resources/static/play/main.js
@@ -1,18 +1,41 @@
 var editor = null
+var file = null
+
+const getLanguage = ext => {
+    const languages = monaco.languages.getLanguages()
+    for (let i = 0; i < languages.length; i++) {
+        const { id, extensions } = languages[i]
+        if (extensions) {
+            if (extensions.indexOf(ext) !== -1) {
+                return id
+            }
+        }
+    }
+
+    return getLanguage('.txt')
+}
+
 var modal = new tingle.modal({
-    footer: true,
+    footer: false,
     stickyFooter: false,
-    closeMethods: ['overlay', 'button', 'escape'],
-    closeLabel: "Close",
-    cssClass: ['custom-class'],
+    closeMethods: ['escape'],
+    cssClass: ['custom-class', 'tingle-modal--fullscreen'],
     onOpen: function () {
         editor = monaco.editor.create(document.querySelector('.custom-class .editor'), {
-            value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-            language: 'javascript'
+            value: file.content,
+            language: getLanguage(file.extension),
+            automaticLayout: true
         });
+
+        document.querySelector('.tingle-btn.tingle-btn--primary').addEventListener('click', () => {
+
+        })
+        document.querySelector('.tingle-btn.tingle-btn--default').addEventListener('click', () => {
+            modal.close()
+        })
     },
     onClose: function () {
-        editor.dispose()
+        editor?.dispose()
         editor = null
     },
     beforeClose: function () {
@@ -20,16 +43,26 @@ var modal = new tingle.modal({
     }
 });
 
-modal.setContent('<div class="editor"></div>');
+modal.setContent(`
+<div class="content">
+        <div class="editor"></div>
+        <div class="buttons tingle-modal-box__footer">
+            <button class="tingle-btn tingle-btn--primary">Save</button>
+            <button class="tingle-btn tingle-btn--default">Cancel</button>
+        </div>
+    </div>`);
 
-// add a button
-modal.addFooterBtn('Save', 'tingle-btn tingle-btn--primary tingle-btn--pull-right', function () {
-    // here goes some logic
-    modal.close();
-});
-
-function showModal() {
-    // open modal
-    modal.open();
-
-}
+document.querySelectorAll('span').forEach(el => el.addEventListener('click', function (e) {
+    fetch(`/play/file?path=${el.getAttribute('data-file')}`).then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            throw new Error("get file content error")
+        }
+    }).then(res => {
+        file = res.data
+        modal.open()
+    }).catch(e => {
+        console.error(e)
+    })
+}))
